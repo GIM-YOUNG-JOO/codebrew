bf58a96babd55558bbe13454db9e2f3dd1bd7797
leek94 <khwan929@naver.com>
2024-04-23 20:19:43 +0900

    feat: mainPage And boardList commit

---
+++ src/main/java/com/mycompany/codebrew/controller/boardController.java
@@ -0,0 +1,18 @@
+package com.mycompany.codebrew.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("board")
+public class boardController {
+	@RequestMapping("/boardList")
+	public String boardList() {
+		log.info("실행");
+		return "board/boardList";
+	}
+
+}

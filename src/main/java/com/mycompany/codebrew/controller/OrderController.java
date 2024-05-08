beec66689666f02a6cbfa86bb7469e4a339ab73d
TaeHwan <xwe6171@gmail.com>
2024-05-08 16:23:53 +0900

    commit push

--- src/main/java/com/mycompany/codebrew/controller/OrderController.java
+++ src/main/java/com/mycompany/codebrew/controller/OrderController.java
@@ -60,4 +60,44 @@ public class OrderController {
 		
 		return "order/menu";
 	}
+	
+	@GetMapping("/cart")
+	public String cart(Principal principal, Model model) {
+		log.info(principal.getName());
+		//일단 유저가 로그인되어 있지 않으면 jsp에서 c:if를 통해서 장바구니 화면을 보여주지 않고 로그인해달라는 페이지로 만들기
+		
+		
+		if(principal.getName() != null) {
+		//유저의 아이디로 찾은 모든 카트를 프로덕트디테일, 프로덕트, 템프옵션, 사이즈옵션, 빈즈옵션 조인해서 가져오기(가져오기전에 필요한 컬럼들로만 구성된 DTO하나 만들어주기)
+			//카트 번호, 프로덕트디테일 번호, 프로덕트 번호, 프로덕트 네임, 프로덕트 인포, 프로덕트 
+		
+		//모델을 통해 화면에 보여줄 수 있게 저장해주기
+		}
+		
+		return "order/cart";
+	}
+	@GetMapping("/detailPage")
+	public String detailPage(Model model, int prId) {
+		log.info("detailPage실행");
+		Product coffee = service.getCoffee(prId);
+		byte[] imageData = coffee.getPrImgData();
+		if (imageData != null) {
+			String img = Base64.getEncoder().encodeToString(imageData);
+			coffee.setPrImageOut(img);
+		}
+		model.addAttribute("coffee", coffee);
+		return "order/detailPage";
+	}
+	@PostMapping("/detailPagePost")
+	public String detailPagePost(ProductDetail productDetail, Principal principal) {
+		log.info("detailPagePost실행");
+		//상품아이디와 원두옵션, 온도옵션, 사이즈옵션, 샷 수량을 받아서  상품디테일 테이블로 넘겨주기
+		service.registProductDetail(productDetail);
+		//카트테이블에 데이터를 넣어주기
+		Cart cart = new Cart();
+		cart.setAcId(principal.getName());
+		cart.setPdId(productDetail.getPdId());
+		service.registCart(cart);
+		return "redirect:/order/menu";
+	}
 }
